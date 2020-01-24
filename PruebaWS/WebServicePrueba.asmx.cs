696361afd893e8ca4d31b1deebc7c3c6cef15ab0
Jonathan <jonathan.chanci@hotmail.com>
2020-01-24 11:25:59 -0500

    Creación del metodo resta

--- PruebaWS/WebServicePrueba.asmx.cs
+++ PruebaWS/WebServicePrueba.asmx.cs
@@ -28,5 +28,11 @@ namespace PruebaWS
         {
             return num1 + num2;
         }
+
+        [WebMethod]
+        public int resta(int num1, int num2)
+        {
+            return num1 - num2;
+        }
     }
 }
